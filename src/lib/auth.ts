f3a484dc11ff13893d92fbc5bd1219fc865a166b
Tatiana-isala <ivverstone@gmail.com>
2025-07-15 15:50:28 +0200

    Delete src directory

--- src/lib/auth.ts
+++
@@ -1,7 +0,0 @@
-// Simple vérification côté client
-export function checkAuth(): boolean {
-    if (typeof window !== 'undefined') {
-      return !!localStorage.getItem('auth-token')
-    }
-    return false
-  }
